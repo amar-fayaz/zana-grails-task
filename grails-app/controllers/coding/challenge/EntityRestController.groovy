678f98716d6eaf4624ff7b6475653afb09274fc3
Amar Fayaz <amar@uni-bremen.de>
2021-01-05 13:32:02 -0500

    Finding content by Title for REST

--- grails-app/controllers/coding/challenge/EntityRestController.groovy
+++ grails-app/controllers/coding/challenge/EntityRestController.groovy
@@ -1,5 +1,6 @@
 package coding.challenge
 
+import grails.converters.JSON
 import grails.rest.RestfulController
 
 class EntityRestController extends RestfulController{
@@ -7,5 +8,23 @@ class EntityRestController extends RestfulController{
 
     EntityRestController() {
         super(Entity)
+    }
+
+    @Override
+    def getObjectToBind() {
+        Map paramsObj = JSON.parse(request)
+        if (paramsObj.contents) {
+            for (int i=0; i<paramsObj.contents.size();i++){
+                if (paramsObj.contents[i].title){
+                    String titleVal = paramsObj.contents[i].title
+                    paramsObj.contents[i] = Content.findByTitle(titleVal)
+                    
+                    if (!paramsObj.contents[i]){
+                        response.sendError(400, "Cannot find entity with the title ${titleVal}")
+                    }
+                }
+            }
+        }
+        paramsObj
     }
 }
