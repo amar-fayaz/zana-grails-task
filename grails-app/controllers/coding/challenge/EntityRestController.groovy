2efa5c0fbfe5b5ca3072b3bc56f233004c7c5a21
Amar Fayaz <amar@uni-bremen.de>
2021-01-05 13:44:40 -0500

    POST and PUT are allowed to create new Items

--- grails-app/controllers/coding/challenge/EntityRestController.groovy
+++ grails-app/controllers/coding/challenge/EntityRestController.groovy
@@ -13,14 +13,21 @@ class EntityRestController extends RestfulController{
     @Override
     def getObjectToBind() {
         Map paramsObj = JSON.parse(request)
+        println paramsObj.dump()
         if (paramsObj.contents) {
             for (int i=0; i<paramsObj.contents.size();i++){
                 if (paramsObj.contents[i].title){
                     String titleVal = paramsObj.contents[i].title
-                    paramsObj.contents[i] = Content.findByTitle(titleVal)
+                    Content returnVal = Content.findByTitle(titleVal)
                     
-                    if (!paramsObj.contents[i]){
-                        response.sendError(400, "Cannot find entity with the title ${titleVal}")
+                    if (!returnVal){
+                        if (request.method == "POST" || request.method == "PUT"){
+                            paramsObj.contents[i] = new Content(paramsObj.contents[i])
+                        } else {
+                            response.sendError(400, "Cannot find entity with the title ${titleVal}")
+                        }
+                    } else {
+                        paramsObj.contents[i] = returnVal
                     }
                 }
             }
